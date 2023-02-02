77bf6adbfa252526a6fa7daa569acbda8f104e84
MSreeN <khyathimnsree369@gmail.com>
2023-02-02 22:49:51 +0530

    added button functionality and form validation

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,20 +1,48 @@
-import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import { useState } from "react";
 
-function AddUser(){
-  function userSubmitHandler(e){
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+
+function AddUser() {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredUserAge,setEnteredUserAge] = useState('');
+  const [isValid, setIsValid] = useState(true)
+
+  function addUserHandler(e) {
     e.preventDefault();
+    if(enteredUsername.trim().length === 0 || enteredUserAge.trim().length === 0){
+      setIsValid(false);
+     return;}
+    if(+enteredUserAge < 1) {
+      setIsValid(false);
+      return;
+    }
+    setEnteredUsername('');
+    setEnteredUserAge('');
+    console.log(enteredUserAge, enteredUsername);
   }
-  return(
-    <Card className = {classes.input}>
-    <form onSubmit={userSubmitHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" name="" value=""/>
-      <label for="">Age (years) </label>
-    <input type="submit" name="" value="number"/>
-    </form>
+
+  function usernameChangeHandler(e){
+    setIsValid(true);
+    setEnteredUsername(e.target.value);
+  }
+  function userAgeChangeHandler(e){
+    setIsValid(true);
+    setEnteredUserAge(e.target.value);
+  }
+
+  return (
+    <Card className={classes.input}>
+      <form  onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input style={{borderColor : !isValid? "red":""}} type="text" name="" value={enteredUsername} onChange={usernameChangeHandler} />
+        <label for="">Age (years) </label>
+        <input style={{borderColor : !isValid? "red":""}} type="number" name="" value={enteredUserAge} onChange={userAgeChangeHandler}/>
+        <Button type = "submit">AddUser</Button>
+      </form>
     </Card>
-  )
+  );
 }
 
 export default AddUser;
